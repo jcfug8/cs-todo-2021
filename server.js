12690cb27493b2d6b1a462b330206fda5a575029
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-04-05 20:53:55 -0600

    add beginning of routing

--- server.js
+++ server.js
@@ -5,14 +5,29 @@ server.use(express.json({}))
 
 server.use(express.static(`${__dirname}/public/`))
 
-server.get("/", (req, res) => {
+server.get("/todo/:id", (req, res) => {
     console.log(req.body)
-    res.send("get request proccessed")
+    res.send(`get request recieved for todo with id ${req.params.id}`)
 })
 
-server.post("/", (req, res) => {
+server.get("/todo", (req, res) => {
     console.log(req.body)
-    res.send("post request proccessed")
+    res.send(`get request recieved to list todos`)
+})
+
+server.post("/todo", (req, res) => {
+    console.log(req.body)
+    res.send(`post request recieved to create todo`)
+})
+
+server.put("/todo/:id", (req, res) => {
+    console.log(req.body)
+    res.send(`put request recieved to replace todo with id ${req.params.id}`)
+})
+
+server.patch("/todo/:id", (req, res) => {
+    console.log(req.body)
+    res.send(`patch request recieved to update todo with id ${req.params.id}`)
 })
 
 module.exports = server

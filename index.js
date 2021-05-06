48050f57d8952d178dbc831d6fe250a0119802ef
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-05-05 21:10:06 -0600

    making small changes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const server = require("./server.js");
 const persist = require("./persist");
 
+// grabs the third item in the command used to start the program as the port
 const port = process.argv[2];
 
+// set up what happends when the app connects to the database
 persist.onConnect(() => {
   server.listen(port, () => {
     console.log(`Code School 2021 Todo App Running on Port ${port}`);
   });
 });
 
+// connect to the database
 persist.connect("127.0.0.1", "cs-todo-2021");
--- persist.js
+++ persist.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 
+/**
+ *
+ * @param {String} host - the hostname for the server running mongo
+ * @param {String} db_name - the name of the database on the mogno instance to be used
+ */
 function connect(host, db_name) {
   mongoose
     .connect(`mongodb://new_user:password@${host}/${db_name}`, {

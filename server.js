19bfe792667b92156ba7356ad924ac1c21592da4
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-05-18 20:51:59 -0600

    forgot something

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const server = express();
 
 const Todo = require("./model");
 
+const getActualRequestDurationInMilliseconds = (start) => {
+  const NS_PER_SEC = 1e9; //  convert to nanoseconds
+  const NS_TO_MS = 1e6; // convert to milliseconds
+  const diff = process.hrtime(start);
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+};
+
 let demoLogger = (req, res, next) => {
   //middleware function
   let current_datetime = new Date();
